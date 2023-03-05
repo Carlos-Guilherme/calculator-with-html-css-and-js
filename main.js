0c7ac426cd2ba5f44c4b6068449712e20f851cee
carlos emanoel <72580077+Carlos-Guilherme@users.noreply.github.com>
2023-03-05 10:19:04 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,94 @@
+function btn1() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML += '1'
+
+}
+
+function btn2() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML += '2'
+
+}
+
+function btn3() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML += '3'
+
+}
+
+function btn4() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML += '4'
+
+}
+
+function btn5() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML += '5'
+
+}
+function btn6() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML += '6'
+
+}
+
+function btn7() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML += '7'
+
+}
+
+function btn8() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML += '8'
+
+}
+
+function btn9() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML += '9'
+
+}
+
+function btn0() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML += '0'
+
+}
+
+function btnsoma() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML += '+'
+
+}
+
+function btnsub() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML += '-'
+
+}
+
+function btnmul() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML += '*'
+
+}
+
+function btndiv() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML += '/'
+
+}
+
+function btndel() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML = ''
+
+}
+
+function btnigual() {
+    element_valor = document.getElementById('id')
+    element_valor.innerHTML = eval(element_valor.innerHTML)
+
+}
